aa28a67ff5470ab87b94ded8233b5bd19b8a7c34
Amit Kumar <127511786+Amit-Kumar8227@users.noreply.github.com>
2023-05-30 15:10:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	let ans=document.getElementById("f1").value;
+	let ans1=document.getElementById("f2").value;
+	alert(ans+" "+ans1);
+	
+	
 
 }
